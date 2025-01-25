87d20eaf5450ea8643c12af07790e55f63dc718a
gamwall <gamwall00@gmail.com>
2025-01-25 02:15:49 +0100

    Update Extract pdf from Website.py

--- Extract pdf from Website.py
+++ Extract pdf from Website.py
@@ -96,7 +96,7 @@ extract_url_pdf(input_url="https://cbic-gst.gov.in/central-tax-notifications.htm
 # https://cbic-gst.gov.in/central-tax-notifications.html
 
 
-# In[ ]:
+extract_url_pdf(input_url="https://juriscassation.cspj.ma/Decisions/RechercheDecisions.html")
 
 
 
